a4f959cfac0a7349e849830cf6d855aa55018124
Cecil Phillip <cecilphillip@stripe.com>
2022-07-01 11:49:22 -0400

    add sample project

---
+++ samples/SampleCheckout/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SampleCheckout.Models;
+using Stripe;
+
+namespace SampleCheckout.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly IStripeClient _stripeClient;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(IStripeClient stripeClient, ILogger<HomeController> logger)
+    {
+        _stripeClient = stripeClient;
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
