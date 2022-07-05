7333d3b1f1f64d0e01868393fc1213a94095c5cb
Cecil Phillip <cecilphillip@stripe.com>
2022-07-05 00:57:19 -0400

    support more events

--- src/Stripe.Extensions.DependencyInjection/Webhooks/WebhookEventProcessor.cs
+++ src/Stripe.Extensions.DependencyInjection/Webhooks/WebhookEventProcessor.cs
@@ -1,3 +1,7 @@
+
+
+using Stripe.Checkout;
+
 namespace Stripe.Extensions.Webhooks;
 
 public interface IWebhookEventProcessor
@@ -12,9 +16,17 @@ public abstract class WebhookEventProcessor: IWebhookEventProcessor
         {
             Events.AccountUpdated => ProcessAccountUpdatedEventAsync(webhookContext, webhookContext.WebhookEvent.Data.Object as Account),
             Events.BalanceAvailable => ProcessBalanceAvailableEventAsync(webhookContext, webhookContext.WebhookEvent.Data.Object as Balance),
+            Events.CheckoutSessionCompleted => ProcessCheckoutSessionCompletedEventAsync(webhookContext, webhookContext.WebhookEvent.Data.Object as Stripe.Checkout.Session),
+            Events.CheckoutSessionExpired  => ProcessCheckoutSessionExpiredEventAsync(webhookContext, webhookContext.WebhookEvent.Data.Object as Stripe.Checkout.Session),
+            Events.CheckoutSessionAsyncPaymentSucceeded  => ProcessCheckoutSessionAsyncPaymentSucceededEventAsync(webhookContext, webhookContext.WebhookEvent.Data.Object as Stripe.Checkout.Session),
+            Events.CheckoutSessionAsyncPaymentFailed  => ProcessCheckoutSessionAsyncPaymentFailedEventAsync(webhookContext, webhookContext.WebhookEvent.Data.Object as Stripe.Checkout.Session),
             _ => Task.CompletedTask
         };
 
+    protected virtual  Task ProcessCheckoutSessionAsyncPaymentSucceededEventAsync(WebhookEventContext webhookContext, Session dataObject)=> Task.CompletedTask;
+    protected virtual  Task ProcessCheckoutSessionAsyncPaymentFailedEventAsync(WebhookEventContext webhookContext, Session dataObject)=> Task.CompletedTask;
+    protected virtual  Task ProcessCheckoutSessionExpiredEventAsync(WebhookEventContext webhookContext, Session dataObject)=> Task.CompletedTask;
+    protected virtual  Task ProcessCheckoutSessionCompletedEventAsync(WebhookEventContext webhookContext, Session dataObject)=> Task.CompletedTask;
     protected virtual Task ProcessAccountUpdatedEventAsync(WebhookEventContext webhookContext, Account? account) => Task.CompletedTask;
     protected virtual Task ProcessBalanceAvailableEventAsync(WebhookEventContext webhookContext, Balance? account) => Task.CompletedTask;
 }
