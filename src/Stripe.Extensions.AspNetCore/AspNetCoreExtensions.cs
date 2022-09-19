1c2d62c1ade5f2381b29dd1512210ce191cd6bad
Cecil Phillip <cecilphillip@stripe.com>
2022-09-19 17:24:07 -0400

    add asp.net core extensions

---
+++ src/Stripe.Extensions.AspNetCore/AspNetCoreExtensions.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Stripe.Extensions.DependencyInjection;
+using Stripe.Extensions.Webhooks;
+
+namespace Stripe.Extensions.AspNetCore;
+
+public static class AspNetCoreExtensions
+{
+    public static IEndpointConventionBuilder MapStripeWebhooks(this IEndpointRouteBuilder endpoints,
+        string path = "/stripe/webhooks", string webhookSecret = null!)
+    {
+        return endpoints.MapPost(path, async (context) =>
+        {
+            var logger = context.RequestServices.GetRequiredService<ILoggerFactory>()
+                .CreateLogger("Stripe.Extensions.AspNetCore");
+
+            var (stripeEvent, error) = await context.Request.ValidateStripeWebhook();
+
+            switch (error)
+            {
+                case StripeException stripeException:
+                    logger.StripeErrorOccurred(stripeException);
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    return;
+                case Exception:
+                    logger.UnableToProcessEvent(error);
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    return;
+            }
+
+            var eventProcessor = context.RequestServices.GetRequiredService<IWebhookEventProcessor>();
+            await eventProcessor.ProcessWebhookAsync(new WebhookEventContext(stripeEvent, context.Request.Headers));
+            context.Response.StatusCode = StatusCodes.Status200OK;
+        });
+    }
+
+    public static Task<(Event stripeEvent, Exception error)> ValidateStripeWebhook(this HttpRequest request,
+        bool throwOnApiVersionMismatch = false)
+    {
+        var stripeOptions = request.HttpContext.RequestServices.GetRequiredService<IOptions<StripeOptions>>();
+        var webhookSecret = stripeOptions.Value.WebhookSecret;
+
+        return request.ValidateStripeWebhook(webhookSecret, throwOnApiVersionMismatch);
+    }
+
+    public static async Task<(Event stripeEvent, Exception error)> ValidateStripeWebhook(this HttpRequest request,
+        string webhookSecret, bool throwOnApiVersionMismatch = false)
+    {
+        try
+        {
+            using var reader = new StreamReader(request.Body);
+            var json = await reader.ReadToEndAsync().ConfigureAwait(false);
+
+            if (!request.Headers.TryGetValue(Constants.STRIPE_WEBHOOK_HEADER_NAME, out var signatureHeader))
+                return (null, new Exception("The webhook secret was not present in the request"));
+
+            EventUtility.ValidateSignature(json, signatureHeader, webhookSecret);
+            var stripeEvent = EventUtility.ParseEvent(json, throwOnApiVersionMismatch);
+            return (stripeEvent, null);
+        }
+        catch (Exception ex)
+        {
+            return (null, ex);
+        }
+    }
+}
--- src/Stripe.Extensions.AspNetCore/CustomLogger.cs
+++ src/Stripe.Extensions.AspNetCore/CustomLogger.cs
@@ -2,18 +2,25 @@ using Microsoft.Extensions.Logging;
 
 namespace Stripe.Extensions.AspNetCore;
 
-[System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
-internal static partial class CustomLogger
-{
-    [LoggerMessage(
-        EventId = 1,
-        Level = LogLevel.Error,
-        Message = "")]
-    public static partial void LogStripeError(ILogger logger, StripeException exception);
 
-    [LoggerMessage(
-        EventId = 3,
-        Level = LogLevel.Error,
-        Message = "Exception processing Stripe webhook event.")]
-    public static partial void LogUnableToProcessEvent(ILogger logger, Exception exception);
+public static class CustomLogger
+{
+    private static readonly Action<ILogger, Exception?> _unableToProcessEvent;
+    private static readonly Action<ILogger,Exception?> _webhookSecretNotConfigured;
+    private static readonly Action<ILogger,string,Exception?> _stripeError;
+
+    static  CustomLogger()
+    {
+        //TODO: We should provide more detail in the StripeError log
+        _stripeError = LoggerMessage.Define<string>(LogLevel.Error, new(1, nameof(StripeErrorOccurred)), "A Stripe error was thrown processing Stripe webhook event: {StripeErrorType}") ;
+        _unableToProcessEvent = LoggerMessage.Define(LogLevel.Error, new(3, nameof(UnableToProcessEvent)), "An Exception was thrown processing Stripe webhook event");
+        _webhookSecretNotConfigured = LoggerMessage.Define(LogLevel.Error, new(5, nameof(WebhookSecretNotConfigured)), "Webhook secret was not provided");
+    }
+
+    public static void StripeErrorOccurred(this ILogger logger, StripeException exception) => _stripeError(logger, exception.StripeError.Type, exception);
+
+    public static  void UnableToProcessEvent(this ILogger logger, Exception exception) => _unableToProcessEvent(logger, exception);
+    
+    public static void WebhookSecretNotConfigured(this ILogger logger) => _webhookSecretNotConfigured(logger, null);
+
 }
