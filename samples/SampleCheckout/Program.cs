07f55d45221d034fb8e839212ad1ee77426d38ef
Cecil Phillip <cecilphillip@stripe.com>
2024-04-28 21:24:30 -0400

    fix samples and test build

--- samples/SampleCheckout/Program.cs
+++ samples/SampleCheckout/Program.cs
@@ -1,30 +1,17 @@
 using Stripe;
-using Stripe.AspNetCore;
+using Stripe.Extensions.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddStripe();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseStaticFiles();
-
 app.UseRouting();
 
-app.UseAuthorization();
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapDefaultControllerRoute();
 app.MapStripeWebhookHandler<MyWebhookHandler>();
 app.Run();
 
@@ -38,7 +25,7 @@ public class MyWebhookHandler: StripeWebhookHandler
 
     public override async Task OnCustomerCreatedAsync(Event e)
     {
-        Customer customer = (Customer)e.Data.Object;
+        var customer = (e.Data.Object as Customer)!;
         await _customerService.UpdateAsync(customer.Id, new CustomerUpdateOptions()
         {
             Description = "New customer"
