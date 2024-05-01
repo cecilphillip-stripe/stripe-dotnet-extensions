25646b82bd3ad43b0756ae9d2ac309fde55cdf63
cecilphillip <cecilphillip@gmail.com>
2024-05-01 01:17:16 -0400

    Use MinVer for package versioning

--- build/Build.cs
+++ build/Build.cs
@@ -1,8 +1,12 @@
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using Nuke.Common;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.MinVer;
 using Nuke.Common.Utilities.Collections;
 using Nuke.Components;
 using Serilog;
@@ -14,22 +18,31 @@ class Build : NukeBuild, IPack, ITest
     public static int Main() => Execute<Build>(x => x.Print);
 
     // string BaseExProjectName => "Stripe.Extensions";
-    //string StripeExDepInjectProjectName => $"{BaseExProjectName}.DependencyInjection";
-    //string StripeExAspNetCoreProjectName => $"{BaseExProjectName}.AspNetCore";
+    // string StripeExDepInjectProjectName => $"{BaseExProjectName}.DependencyInjection";
+    // string StripeExAspNetCoreProjectName => $"{BaseExProjectName}.AspNetCore";
+    
+    MinVer MinVerInfo { get; set; }
+    [GitRepository] GitRepository GitRepo { get; }
+
+    [Parameter] readonly string MinVerTagPrefix;
+    [Parameter] readonly string MinVerPreReleaseIdentifiers;
+    [Parameter] readonly string MinVerMinimumMajorMinor;
 
     AbsolutePath SourceDirectory => RootDirectory / "src";
     AbsolutePath TestsDirectory => RootDirectory / "tests";
     AbsolutePath SamplesDirectory => RootDirectory / "samples";
-    
-    
+
+
     Target Print => target => target
         .Unlisted()
         .Description("Prints resolved project metadata")
         .Executes(() =>
         {
             var solution = (this as IHazSolution).Solution;
-            Log.Information("Solution path = {Value}", solution.Path);
-            Log.Information("Solution directory = {Value}", solution.Directory);
+            Log.Information("Solution path = {Path}", solution.Path);
+            Log.Information("Solution directory = {Dir}", solution.Directory);
+            Log.Information("Source Repository = {GitUrl} / [{GitBranch}]", GitRepo.HttpsUrl, GitRepo.Branch);
+            Log.Information("Project Version {Version}", MinVerInfo.Version);
         });
 
     Target CleanSamples => target => target
@@ -60,11 +73,29 @@ class Build : NukeBuild, IPack, ITest
     Target Clean => target => target
         .Description("Clean all projects directories")
         .DependsOn(CleanSamples, CleanTests, CleanSource)
+        .TryDependentFor<IRestore>()
         .Executes(() =>
         {
             (this as IHazArtifacts).ArtifactsDirectory
                 .CreateOrCleanDirectory();
         });
-    
+
     public IEnumerable<Project> TestProjects => (this as IHazSolution).Solution.GetAllProjects("*.Tests");
+
+    public Configure<DotNetPackSettings> PackSettings => settings => settings
+        .SetVersion(MinVerInfo.Version)
+        .SetRepositoryUrl(GitRepo.HttpsUrl);
+    
+    protected override void OnBuildInitialized()
+    {
+        base.OnBuildInitialized();
+        var version = MinVerTasks.MinVer(s => s
+                .SetTagPrefix(MinVerTagPrefix)
+                .SetMinimumMajorMinor(MinVerMinimumMajorMinor)
+                .SetProcessEnvironmentVariable("MinVerDefaultPreReleaseIdentifiers", MinVerPreReleaseIdentifiers)
+                .DisableProcessLogOutput())
+            .Result;
+
+        MinVerInfo = version;
+    }
 }
