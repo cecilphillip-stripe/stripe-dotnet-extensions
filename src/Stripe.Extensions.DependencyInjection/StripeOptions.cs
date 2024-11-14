17e680544fcc83ce962b34d7234c203cca624c1b
cecilphillip <cecilphillip@gmail.com>
2024-11-14 06:26:26 -0800

    support StripeClientOptions

--- src/Stripe.Extensions.DependencyInjection/StripeOptions.cs
+++ src/Stripe.Extensions.DependencyInjection/StripeOptions.cs
@@ -1,14 +1,15 @@
 namespace Stripe.Extensions.DependencyInjection;
 
-public class StripeOptions
+public class StripeOptions: StripeClientOptions
 {
-    public const string DefaultClientConfigurationSectionName = "Stripe";
+    public const string DefaultClientConfigurationSectionName = "Default";
     public string ClientName { get; set; } = string.Empty;
     public string PublicKey { get; set; } = string.Empty;
-    public string SecretKey { get; set; } = string.Empty;
+    public string SecretKey => ApiKey;
     public string WebhookSecret { get; set; } = string.Empty;
     public bool ThrowOnWebhookApiVersionMismatch { get; set; } = true;
     public bool EnableTelemetry { get; set; } = true;
     public AppInfo? AppInfo { get; set; }
+    public string ApiVersion => StripeConfiguration.ApiVersion;
     public int MaxNetworkRetries { get; set; } = SystemNetHttpClient.DefaultMaxNumberRetries;
 }
